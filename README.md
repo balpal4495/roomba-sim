1e89043498e3e192ded2f57a4a0f970009b43711
baljitp <baljit.pal@chelsea-apps.com>
2017-05-01 21:13:53 +0100

    simple start up

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # roomba-sim
+HTML based Roomba simulator
+
+install dependencies
+```
+npm install
+```
+
+Start server and go to http://localhost:3000
+```
+npm run start
+```
---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.set('port', 3000);
+app.use(express.static(path.join(__dirname)));
+app.get('/', (req, res) => res.sendFile(path.join(`${__dirname}/index.html`)));
+// Listen for requests
+const server = app.listen(app.get('port'), () => {
+  const port = server.address().port;
+  console.log(`Server listening on ${port}`);
+});
